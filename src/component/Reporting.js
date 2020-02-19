3340dd6dabdf4c79e135f9738e1d491b41e3ee68
Olufemi Oluoje <kobomarun@gmail.com>
2020-02-19 17:17:13 +0100

    working version

---
+++ src/component/Reporting.js
@@ -0,0 +1,148 @@
+import React, { Component } from "react";
+// import data from '../data.json'
+import '../Style.css'
+
+class Reporting extends Component {
+    constructor(props) {
+        super(props);
+        this.state =  {
+            posts: [],
+            count: 0,
+            name: '',
+            data:[],
+            distinct:[],
+            list: false,
+            selected: null
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://rp.54gene.com/api/get_participants')
+        .then(response => response.json())
+        .then(json => this.setState({posts:json.data}))
+        .catch(err => console.log('fetch error:', err))
+        this.setState({distinct: this.state.posts})
+    }
+
+     getUnique = (array) => {
+        let uniqueArray = [];
+
+        for(let i=0; i < array.length; i++){
+            if(uniqueArray.indexOf(array[i]) === -1) {
+                uniqueArray.push(array[i]);
+            }
+
+        }
+
+        return uniqueArray;
+    }
+    
+
+    selectParticipantInfo(e) {
+        console.log(e.target.value)
+        const response = e.target.value
+        let allSelected = []
+        this.state.posts.map((item, i) => {
+            Object.entries(item).map(list => {
+                allSelected.push(list)
+            })
+        })
+        const filterdData = allSelected.filter(dd => {
+            if(dd[0] === response) {
+                return dd[1]
+            }
+        })
+        // console.log('sele',allSelected)
+        this.setState({name: response, 
+            count: allSelected.length, 
+            distinct: this.getUnique(allSelected),
+            data: filterdData,
+            list: true,
+            selected: response
+        })
+
+        
+        console.log('dfdf',this.getUnique(filterdData))
+    }
+
+    
+
+    render() {
+        const { posts, data, distinct , selected} = this.state
+        return(
+            <React.Fragment>
+                <div className="container page middle">
+                <h1 className="jumbotron">Reporting Participant Information</h1>
+                
+                <div className="row">
+                    <div className="col-md-12">
+                    <table className="table">
+                    {/* <thead className="thead-dark">
+                        <tr>
+                            <th>Question</th>
+                            <th>Response</th>
+                        </tr>
+                    </thead> */}
+                       
+                                    
+                                        <tr>
+                                            <td>
+                                            <select name="prt" className="form-cntrol" onChange={(e) => this.selectParticipantInfo(e)}>
+                                               <option value="">Select Participant Information</option> 
+                                               {
+                                                   this.state.posts.map((list, i) => (
+                                                       this.getUnique(Object.entries(list)).map(item => {
+                                                        return(
+                                                            <option value={item[0]}>{item[0]}</option>
+                                                        )
+                                                       })
+                                                       
+                                                    ))
+                                                        
+                                                }
+                                            </select></td>
+                                                                                   {/* <td style ={{fontSize:12, cursor:'pointer'}} onClick={() => this.getQuestion(item[1])}>{item[0]}</td>
+                                        <td style ={{fontSize:12, cursor:'pointer'}}>{item[1]}</td> */}
+                                    </tr>
+                           
+                    </table>
+                    </div>
+                    {/* <div className="col-md-6">
+                    <select className="form-cntrol" onChange={(e) => this.selectTodo(e)}>
+                        <option value="">What Do you want to do</option>
+                            { this.state.distinct.map(row => (
+                                <option value={row}>{row}</option>
+
+                            ))}
+                            <option value="list">List All </option>
+                        </select>
+                    </div> */}
+                </div>
+                </div>
+                {this.state.list ? 
+                <div className="container page">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h2 className="h4">List view for {this.state.name}. Total: <span className="badge"> {this.state.count}</span></h2>
+                        <div className="num_count badge">{`${this.state.name}(${this.state.count})`}</div>
+                            <ul className="list-group">
+                                { this.getUnique(data).map(row => {
+                                    return(
+                                        <li className="list-group-item">{row[1]} <span className="badge">{row[1].length}</span></li>
+
+                                    )
+                                })
+                                
+                                }
+                            </ul>
+                    </div>
+                </div>
+                </div>
+               : <div></div>}
+            </React.Fragment>
+            
+        )
+    }
+}
+
+export default Reporting;
